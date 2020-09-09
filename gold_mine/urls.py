2a073f6e13f10b730552af0cf391dd0d4fc1e014
clark <clark.eustaquio@gmail.com>
2020-09-09 17:32:40 +0800

    repath admin url

--- gold_mine/urls.py
+++ gold_mine/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-panel/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('gold_app.urls')),
 ]
