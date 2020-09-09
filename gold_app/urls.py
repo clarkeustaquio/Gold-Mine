28d6caedb7fb3d53a8cbb1e7ce72ec717646d426
clark <clark.eustaquio@gmail.com>
2020-09-09 20:07:41 +0800

    Remove random urls and make changes in leaderboard

--- gold_app/urls.py
+++ gold_app/urls.py
@@ -1,20 +1,14 @@
 from django.urls import path
 from . import views
-from random import random
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 app_name = 'gold_app'
 
-def generate_random():
-    random_number = str(random())
-    random_number = random_number.replace(".", "")
-    return random_number
-
 urlpatterns = [
     path('', views.index, name='index'),
-    path('user_select/{}<int:id>{}/'.format(generate_random(), generate_random()), views.user_select, name='user_select'),
+    path('user_select/<int:id>/', views.user_select, name='user_select'),
     path('leaderboard/', views.leader_board, name='leader_board'),
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
